da8ab741e466d6168c8ab873896d256a87d02494
Karen Palacio <karen.palacio.1994@gmail.com>
2020-02-09 23:58:34 -0300

    Casi ahí. Hay que debuggear

--- fuzzymatch/fuzzmatch.py
+++ fuzzymatch/fuzzmatch.py
@@ -4,19 +4,33 @@ import numpy as np
 word1=sys.argv[1]
 word2=sys.argv[2]
 
-def create_matr(word1,word2):
+def create_matrix(word1, word2, m, n):
+    lmatrix = np.arange((m+1)*(n+1)).reshape(m+1,n+1)
+    for i in range(m+1):
+        lmatrix[i][0]=i
+    for j in range(n+1):
+        lmatrix[0][j]=j
+    return lmatrix
+
+#lmatrix = create_matrix(word1,word2)
+#print(lmatrix)
+
+
+def levenshtein(word1, word2):
     m = len(word1)
     n = len(word2)
-    matrix = np.arange().reshape(m+1,n+1)
-    for i in range(m):
-        matrix[][]=i
-    for j in range(n):
-        matrix[][]=j
-    return matrix
+    lmatrix = create_matrix(word1,word2,m,n)
+    for i in range(1,lx):
+        for j in range(1,ly):
+            if min(i,j) == 0:
+                lmatrix[i][j]=max(i,j)
+            else:
+                factor = 1 if word1==word2 else 0
+                lmatrix[i][j]=min(lmatrix[i-1,j]+1,lmatrix[i][j-1]+1,lmatrix[i-1,j-1]+factor)
+    return lmatrix    
 
-def levenshtein(word1, word2,i,j):
-    matrix = creat_matr(word1,word2)
-    if min(i,j) == 0:
-        return max(i,j)
 
-res = levenshtein(word1,word2, len(word1)+1, len(word2)+1)
+res_matrix = levenshtein(word1,word2)
+#print(res_matrix.shape, len(word1)+1, len(word2)+1)
+distance = res_matrix[len(word1)][len(word2)]
+print(distance)
